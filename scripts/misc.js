e10b5de0e0372a85f13ca8dedc0bb367059071ae
Esteban Borai <estebanborai@outlook.com>
2018-04-29 21:18:03 -0300

    Add About Section Technologies

--- scripts/misc.js
+++ scripts/misc.js
@@ -1,28 +1,6 @@
-function randomInteger(max, min) {
-  return Math.floor((Math.random() * max) + min);
-}
-
-const frontWords = [
-  'Front',
-  'Fall',
-  'Free',
-  'For',
-  'Fallout',
-  'Forever'
-];
-
-const endWords = [
-  'End',
-  'Everest',
-  'Ever',
-  'Evil',
-  'Ernesto',
-  'Event'
-];
-
 const slidesSet = [
   {
-    text: 'Test',
+    text: 'Learn Front-End & JavaScript Development',
     img: 'assets/slider/001.jpg'
   }
 ];
@@ -46,11 +24,6 @@ function createSlides(slides) {
     text.className = 'fade-in';
     text.innerText = slide.text;
 
-    let slideIndex = document.createElement('span');
-    slideIndex.innerText = `${index + 1}/${slides.length}`;
-    slideIndex.className = 'slide-counter fade-out';
-    text.appendChild(slideIndex);
-
     slideContainer.appendChild(text);
 
     target.appendChild(slideContainer);
@@ -58,7 +31,5 @@ function createSlides(slides) {
 }
 
 (function(){
-  const title = document.getElementById('title');
-  title.innerText = findTitle(frontWords, endWords);
   createSlides(slidesSet);
 })();
